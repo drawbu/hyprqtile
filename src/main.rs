0f17d92d7aff7c1f7d9a73339175af45314086d6
Clément <clement2104.boillot@gmail.com>
2024-07-05 21:52:08 +0200

    fix: move to inactive workspace

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,36 @@
 mod cli;
 
 use anyhow::bail;
-
-use hyprland::data::{Monitor, Workspace, Workspaces};
-use hyprland::dispatch::{
-    Dispatch, DispatchType, MonitorIdentifier, WorkspaceIdentifierWithSpecial,
-};
 use hyprland::shared::{HyprData, HyprDataActive};
+use hyprland::{
+    data::{Monitor, Workspaces},
+    dispatch::{Dispatch, DispatchType, MonitorIdentifier, WorkspaceIdentifierWithSpecial},
+};
 
 /// Move window to a workspace
 ///
 /// If there the workspace is active on a window, switch the two windows
-fn move_to_workspace(t_workspace: &Workspace) -> anyhow::Result<()> {
-    cli::log(&format!("move on monitor {:#?}", t_workspace));
+fn move_to_workspace(workspace_id: i32) -> anyhow::Result<()> {
+    let workspaces = Workspaces::get()?;
+    let target = match workspaces.iter().find(|w| w.id == workspace_id) {
+        Some(w) => w,
+        None => {
+            cli::log(&format!("move to inactive workspace {:#?}", workspace_id));
+            hyprland::dispatch!(Workspace, WorkspaceIdentifierWithSpecial::Id(workspace_id))?;
+            return Ok(());
+        }
+    };
+    cli::log(&format!("move to active workspace {:#?}", target));
 
-    if t_workspace.monitor_id == Monitor::get_active()?.id {
-        hyprland::dispatch!(
-            Workspace,
-            WorkspaceIdentifierWithSpecial::Id(t_workspace.id)
-        )?;
-        return Ok(())
+    if target.monitor_id == Monitor::get_active()?.id {
+        hyprland::dispatch!(Workspace, WorkspaceIdentifierWithSpecial::Id(target.id))?;
+        return Ok(());
     }
 
     hyprland::dispatch!(
         SwapActiveWorkspaces,
         MonitorIdentifier::Current,
-        MonitorIdentifier::Id(t_workspace.monitor_id)
+        MonitorIdentifier::Id(target.monitor_id)
     )?;
     Ok(())
 }
@@ -35,14 +40,7 @@ fn main() -> anyhow::Result<()> {
     cli::log(&format!("args: {:#?}", args));
 
     match &args.command {
-        Some(cli::Commands::Move { target }) => {
-            let workspaces = Workspaces::get()?;
-            let workspace = match workspaces.iter().find(|w| w.id == *target) {
-                Some(w) => w,
-                None => bail!("workspace not found"),
-            };
-            move_to_workspace(&workspace)?;
-        }
+        Some(cli::Commands::Move { target }) => move_to_workspace(*target)?,
         None => bail!("No command provided"),
     }
     Ok(())
