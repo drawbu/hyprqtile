6a947972e306a2e0799470533b8592a4cb22ab1a
Clément <clement2104.boillot@gmail.com>
2024-10-27 19:30:22 +0100

    chore: update dependencies

--- flake.nix
+++ flake.nix
@@ -4,25 +4,38 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {self, ...} @ inputs:
-    inputs.utils.lib.eachDefaultSystem (system: let
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
-    in {
-      formatter = pkgs.alejandra;
+  outputs =
+    { self, ... }@inputs:
+    inputs.utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixfmt-rfc-style;
 
-      devShells.default = pkgs.mkShell {
-        RUSTC_VERSION = "stable";
-        packages = with pkgs; [rustup];
-      };
-
-      packages = {
-        default = self.packages.${system}.hyprqtile;
-        hyprqtile = pkgs.rustPlatform.buildRustPackage {
-          pname = "hyprqtile";
-          version = "0.1.0";
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.packages.${system};
+          env = rec {
+            RUST_LOG = "info";
+            RUSTC_VERSION = pkgs.rustc.version;
+            RUSTUP_TOOLCHAIN = RUSTC_VERSION;
+          };
+          packages = with pkgs; [
+            rust-analyzer
+            clippy
+          ];
         };
-      };
-    });
+
+        packages = {
+          default = self.packages.${system}.hyprqtile;
+          hyprqtile = pkgs.rustPlatform.buildRustPackage {
+            pname = "hyprqtile";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+        };
+      }
+    );
 }
